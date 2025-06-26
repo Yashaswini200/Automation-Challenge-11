99902eddab3ef27a246a486a2fe8e74399c41145
Yashaswini200 <yashusuresh2050@gmail.com>
2025-06-26 20:05:58 +0530

    commit

---
+++ src/main/java/org/example/Utlis/WaitHelpers.java
@@ -0,0 +1,16 @@
+package org.example.Utlis;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitHelpers {
+
+    public static void checkVisibility(WebDriver driver, By locator,int time){
+        WebDriverWait webDriverWait=new WebDriverWait(driver, Duration.ofSeconds(time));
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+}
